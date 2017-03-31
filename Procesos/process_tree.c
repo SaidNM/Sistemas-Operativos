aeabdeb6e53c543097c91f1ca298918a934a5e71
saidnm <saidnm044@hotmail.com>
2017-03-31 08:45:19 -0600

    Creo que ya quedo

--- Procesos/process_tree.c
+++ Procesos/process_tree.c
@@ -2,29 +2,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void crear_hijo(int nivel,int num_hijos, int pos, int status){
+void crear_hijo(int nivel,int num_hijos, int pos, int status, int paro, int hijos){
+	if(paro==nivel){
+		exit(0);
+	}
 	for(int i=0;i<num_hijos;i++){
 	int pid1=fork();
 	int status=0;
 	if(pid1>0){
-		if(pos==0){
+		/*if(pos==0){
 			printf("Soy el padre - mi pid es %d -mi nivel es %d , ---Izquierdo \n",getpid(),nivel);
 		}
 		else{
 			printf("Soy el padre - mi pid es %d -mi nivel es %d ,---Derecho \n",getpid(),nivel);
-		}
+		}*/
 		while(wait(&status)>0);
 	}
-	else{
+	else if(pid1==0){
 		nivel++;
+		hijos++;
 		if(pos==0){
 			printf("\t Soy hijo -mi pid es %d - mi padre es %d - mi nivel es %d ---Izquierdo \n",getpid(),getppid(),nivel);	
 			}
 		else{
-		printf("\t Soy hijo -mi pid es %d - mi padre es %d - mi nivel es %d  ---Derecho \n",getpid(),getppid(),nivel);
+		printf("\t \t Soy hijo -mi pid es %d - mi padre es %d - mi nivel es %d  ---Derecho \n",getpid(),getppid(),nivel);
 			}
+		crear_hijo(nivel,num_hijos,pos,status,paro,hijos);
 		break;
+		
 		}
+	else{
+		printf("\nError");
+		exit(1);
+	}
 	}
 } 
 //---------------------------------------------------------------------------------------------------------------
@@ -33,6 +43,8 @@ int main(int argc, char const *argv[])
  	int pid=0;
  	int nivel=0;
  	int status=0;
+ 	int paro=0;
+ 	int hijosc=0;
  	int num_hijos_izq=0;
  	int num_hijos_der=0;
 
@@ -45,6 +57,12 @@ int main(int argc, char const *argv[])
  	printf("\nNum hijos derecho: ");
  	scanf("%d",&num_hijos_der);
  	if(num_hijos_der<0){
+ 		printf("\nError");
+ 		exit(1);
+ 	}
+ 	printf("\nNiveles: ");
+ 	scanf("%d",&paro);
+ 	if(paro<0){
  		printf("\nError");
  		exit(1);
  	}
@@ -61,12 +79,12 @@ int main(int argc, char const *argv[])
 	 		if(i==0){ //Es el hijo izquierdo
 	 			nivel++;
 	 			printf("\t Soy el hijo izquierdo -mi pid es %d - mi padre es %d - mi nivel es %d \n",getpid(),getppid(),nivel);
-	 			crear_hijo(nivel,num_hijos_izq,i,status);
+	 			crear_hijo(nivel,num_hijos_izq,i,status,paro,hijosc);
 	 		}
 	 		else{//Es el hijo derecho
 				nivel++;
 	 			printf("\t Soy el hijo derecho -mi pid es %d - mi padre es %d - mi nivel es %d \n",getpid(),getppid(),nivel);
-	 			crear_hijo(nivel,num_hijos_der,i,status);
+	 			crear_hijo(nivel,num_hijos_der,i,status,paro,hijosc);
 	 		}
 	 		break;
 	 	}
